2b0c16f1a7987988ad3ef0d362c24441e8e528f9
LakshaySanhotra <lakshaysanhotra1@gmail.com>
2023-11-26 21:09:53 +0530

    price added

--- public/js/main.js
+++ public/js/main.js
@@ -3,56 +3,56 @@ const products = [
     {
         id: 1,
         title: "Nike Jumpman",
-        price: 264.9,
+        price: 2640.9,
         image:
             "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiJmx9wj6ILk6sbU6lNu1fM_zsF64OnR0aQHsuDpicjrQmQUpegoA2g6MAoIUQQSGL36o8Ta_WSXZYQMj3f0dIM_6LbAy8WJHYGmnU5mV_izj4RsNRmRpTrNRIwZy65CevTGhmDuqvT32yFX1te9Q-VALdetJsHOp4bw9dTJqQQm9XvsJpasM5XKxTZ/s320/shoe1.webp",
     },
     {
         id: 2,
         title: "Nike Dunk High",
-        price: 295,
+        price: 2950,
         image:
             "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjJvpx0YyAOjo80ZOe6yg7iHkR_R9wQqUW4onK_r_hKx546Gz0cQl6L21xICxgp3Mxaicfn6gZN59koDg85QJlqDJF3ad6FBQTEIPU5D6PZioKpqujYoREllUDjAV9IClh-5H9Xkr0tvgYcxo5PYUAWWXvxLrCBQT51L2xYxuhQ6l-gT9dTTNYINLby/s320/shoe2.webp",
     },
     {
         id: 3,
         title: "Nike Air Jordan",
-        price: 74.9,
+        price: 740.9,
         image:
             "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhFQvZevXm-Oyld6eSob5-83_lyj0lhqiFn-l9OjGXFyMAk4fDcFuyjc9Yvy4V04HV5VCkNI82RyT9w0ePt2wEkBlIammvFMAilU_JOigKJEXcYWNwYcmrdoNR1H0b6VThiH1pK6PNb75byVEBX4gwfAU440zS2BZHm75D4nsu-NNkAJhf5SzmxN9tf/s320/shoe3.webp",
     },
     {
         id: 4,
         title: "Nike Air Max SYSTM",
-        price: 80,
+        price: 800,
         image:
             "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhTCToBc5mAQozH9BWpfdI-aNxKn4MGlDhz5gU3ImGbCcIMu1fxAYD-cHRN1WJvd-qey3Uu4SiQ-D1ni0hYW3RoH3o1wXRuIcKsc1DYVqm7PgqDUjjqp6msVdLV-EKLjxQRuPBrihxXvh54nKOEjfukWLKPQQepjnM34TcWbzuJo6oSU4cM-QeghbYd/s320/shoe4.webp",
     },
     {
         id: 5,
         title: "Nike Air Force",
-        price: 48.99,
+        price: 480.99,
         image:
             "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgeGXR6jWsel2QFq3wZ9njOaw3q25-i2jhKl26Uwv6-_xdJGzVGMAXPuq7jpAmdqE22C3IMuOtJVM6SP5N4XOZlrQX6DIsg2K2rwXXetDlzKPhdkGkij4zLkRG0W-jHLuMArIO4hKhLrr8pL1S1SCUvRrBWe5Wbr-RRmCnZWAaNvtQ5qcTCeorOeE1Y/s320/shoe5.webp",
     },
     {
         id: 6,
         title: "Nike Vaporfly",
-        price: 395,
+        price: 3950,
         image:
             "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh0le0z-BJdvzEI7pU6tcrpY8MG9VpSzQiW0117PO9g1nx9fBg7-6EMFRR_AgOMZUd2n1MptxMSbweTOwzyPrvo-badBHyBqmOj5zsmmXzLp2xuM53jZrI3q8-n1fFMYS9YQr1RtnNKFtwouXZ3bgvTjLCu1CNUHM6PRZiZq1gyZHEnBTN0Va2ip0qE/s320/shoe6.webp",
     },
     {
         id: 7,
         title: "Nike Zegama",
-        price: 48.99,
+        price: 4800.99,
         image:
             "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEguJPgD3RrfozaOaThl0HW_2YFw8LmB9bWCPLuodcXNYWB4oYhKPZjnhS2EhG45lOlJAu4XCrcmSNjJZVq7XSUne27odG6KzcmTxm3gIINuDDtbbraPK8Lv9lWv7tiT7nk6sEXbNusWK-osZshqRBMKRDhEvDa8-URfLs6nELGJigI-IT4dQj1__JDq/s320/shoe7.webp",
     },
     {
         id: 8,
         title: "Nike Kiger 9",
-        price: 79.99,
+        price: 7900.99,
         image:
             "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhf6s7e1roZhfmYaljrttFPtHKOnvCoAzDkBTqaurT7gxfggdAyMSKV4XB9OLiZ6ljNNR6BJNimtaYiE1wLh-baZdddQ1z4lLLK5saNUoltbDrkPylqGjGxSxEGhNHyFh9NFikt0YPwuRqpXv8H2mND9JX2BEwWnJKPNI25lxgEocAsfyhw-YeMVRTN/s320/shoe8.webp",
     },
